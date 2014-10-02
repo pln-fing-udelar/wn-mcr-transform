919e19f589d67624b3ea638e183ccb434ab71a48
Santiago Castro <santi.1410@hotmail.com>
2014-10-02 14:48:34 -0300

    minor changes

--- generate_all.sh
+++ generate_all.sh
@@ -2,18 +2,19 @@
 
 echoerr() { echo "$@" 1>&2; }
 
-if [ "$#" -lt 3 -o "$#" -gt 4 ]; then
+if [ "$#" -lt 2 -o "$#" -gt 3 ]; then
 	echoerr "Incorrect number of parameters. Syntax should be:"
-	echoerr "    $ $0 root_mcr root_eng root_result [foreign_glosses_path]"
+	echoerr "    $ $0 root_mcr root_eng [foreign_glosses_path]"
 else
 	ROOT_MCR=${1%%/}
 	ROOT_ENG=${2%%/}
-	ROOT_RESULT=${3%%/}
-	FOREIGN_GLOSSES_PATH=${4%%/}
+	ROOT_RESULT=aux
+	FOREIGN_GLOSSES_PATH=${3%%/}
+	mkdir $ROOT_RESULT
 	if [ ! -d $ROOT_RESULT -o "$(ls -A $ROOT_RESULT)" ]; then
 	    echoerr "\"$ROOT_RESULT\" directory does not exists or is not empty"
 	else
-		for LANG in `find $ROOT_MCR -iregex '.*WN' -printf '%f ' | sed -e "s/\(\w\)WN/\1/g"`; do #'cat' 'eng' 'eus' 'glg' 'spa'; do
+		for LANG in `find $ROOT_MCR -iregex '.*WN' -printf '%f ' | sed -e "s/\(\w\)WN/\1/g"`; do
 			echo "-------------------"
 			echo "Processing $LANG..."
 			echo "-------------------"
@@ -29,4 +30,5 @@ else
 		done
 		echo "Done"
 	fi
+	rmdir $ROOT_RESULT
 fi
